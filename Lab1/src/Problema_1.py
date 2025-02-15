2b176d32a6d4a708ef9a56eb5668612751a1fd26
Jose-Fabian-Molina <jose.molinaez@udlap.mx>
2025-02-15 12:25:16 -0600

    1st Solution

--- Lab1/src/Problema_1.py
+++ Lab1/src/Problema_1.py
@@ -1,1 +1,10 @@
+def main():
 
+    n = int(input("Introduce un entero positivo: "))
+
+    suma = n * (n + 1) // 2 
+
+    print(f"La suma de los primeros {n} enteros positivos es: {suma}")
+
+if __name__ == "__main__":
+    main()
